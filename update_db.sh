5bea25170cee97f94fa5e89b279dd3bac3fcd86e
Grisuno <grisuno@gmail.com>
2024-05-21 00:28:17 -0400

    Create update_db.sh
    ██╗      █████╗ ███████╗██╗   ██╗ ██████╗ ██╗    ██╗███╗   ██╗
    ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██╔═══██╗██║    ██║████╗  ██║
    ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║ █╗ ██║██╔██╗ ██║
    ██║     ██╔══██║ ███╔╝    ╚██╔╝  ██║   ██║██║███╗██║██║╚██╗██║
    ███████╗██║  ██║███████╗   ██║   ╚██████╔╝╚███╔███╔╝██║ ╚████║
    ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝    ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝

---
+++ update_db.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Banner
+echo "██╗      █████╗ ███████╗██╗   ██╗ ██████╗ ██╗    ██╗███╗   ██╗"
+echo "██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██╔═══██╗██║    ██║████╗  ██║"
+echo "██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║ █╗ ██║██╔██╗ ██║"
+echo "██║     ██╔══██║ ███╔╝    ╚██╔╝  ██║   ██║██║███╗██║██║╚██╗██║"
+echo "███████╗██║  ██║███████╗   ██║   ╚██████╔╝╚███╔███╔╝██║ ╚████║"
+echo "╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝    ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝"
+
+# Ejecuta search.py
+echo "[*] Ejecutando search.py..."
+python3 search.py
+
+# Verifica si search.py se ejecutó correctamente
+if [ $? -eq 0 ]; then
+    echo "[+] search.py ejecutado con éxito."
+    echo "[*] Ejecutando detailed_search.py..."
+    python3 detailed_search.py
+
+    # Verifica si detailed_search.py se ejecutó correctamente
+    if [ $? -eq 0 ]; then
+        echo "[+] detailed_search.py ejecutado con éxito."
+        echo "[*] Ejecutando lazyown.py..."
+        python3 lazyown.py
+    else
+        echo "[-] Error al ejecutar detailed_search.py."
+        exit 1
+    fi
+else
+    echo "[-] Error al ejecutar search.py."
+    exit 1
+fi

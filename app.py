d6b5355b13fcc37a906a81704d0f94f7e7c6a5c1
56964 <grisuno@gmail.com>
2024-06-09 21:34:32 -0400

    new tags
    new tags

--- app.py
+++ app.py
@@ -270,7 +270,7 @@ class LazyOwnShell(Cmd):
         if not rhost or not rport or not rat_key:
             print("[?] rhost and lport and rat_key must be set")
             return
-        self.run_script("modules/lazybotnet.py", "--host", rhost, "--port", str(rport), "--key", rat_key, " --botnet-file botnet.txt --log-file keylog.log" )
+        self.run_script("modules/lazybotnet.py", "--host", rhost, "--port", str(rport), "--key", rat_key )
 
     def run_lazybotcli(self):
         rhost = "0.0.0.0"

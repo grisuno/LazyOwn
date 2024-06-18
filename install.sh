248739cd6e75c45bd4761727074977495ace9e48
gris <gris@localhost.localdomain>
2024-06-18 17:46:55 -0400

    fix install

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
- #!/bin/bash
- 2 sudo apt update
- 3 sudo apt install ltrace
- 4 python3 -m venv env
- 5 source env/bin/activate
- 6 pip3 install pwn
- 7 pip3 install groq
- 8 pip3 install PyPDF2
- 9 pip3 install docx
-10 pip3 install python-docx
-11 pip3 install olefile
-12 pip3 install exifread
-13 pip3 install pycryptodome
-14 pip3 install impacket
-15 pip3 install pandas
-16 pip3 install colorama
-17 pip3 install tabulate
-18 pip3 install pyarrow
+#!/bin/bash
+sudo apt update
+sudo apt install ltrace
+python3 -m venv env
+source env/bin/activate
+pip3 install pwn
+pip3 install groq
+pip3 install PyPDF2
+pip3 install docx
+pip3 install python-docx
+pip3 install olefile
+pip3 install exifread
+pip3 install pycryptodome
+pip3 install impacket
+pip3 install pandas
+pip3 install colorama
+pip3 install tabulate
+pip3 install pyarrow

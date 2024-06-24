1449399172220218fe1b8eb8115458ba6d9c899c
Grisuno <grisuno@gmail.com>
2024-06-24 16:54:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,17 +3,8 @@ sudo apt update
 sudo apt install ltrace
 python3 -m venv env
 source env/bin/activate
-pip3 install pwn
-pip3 install groq
-pip3 install PyPDF2
-pip3 install docx
-pip3 install python-docx
-pip3 install olefile
-pip3 install exifread
-pip3 install pycryptodome
-pip3 install impacket
-pip3 install pandas
-pip3 install colorama
-pip3 install tabulate
-pip3 install pyarrow
-git clone --depth=1 https://github.com/grisuno/LazyOwnInfiniteStorage.git ./modules_ext/ 
+pip install -r requirements.txt
+git clone --depth=1 https://github.com/grisuno/LazyOwnInfiniteStorage.git ./modules_ext/lazyown_infinitestorage
+chmod +x /modules_ext/lazyown_infinitestorage/install.sh
+/modules_ext/lazyown_infinitestorage/
+./install.sh

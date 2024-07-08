7905df7ece45c5cc6ea9c15b96de3c1689ad75ca
gris <gris@localhost.localdomain>
2024-07-08 18:22:38 -0400

    2 new exploit external :D working like a charm

--- external/install_external.sh
+++ external/install_external.sh
@@ -2,3 +2,5 @@
 git clone https://github.com/amriunix/CVE-2007-2447.git ./exploits/CVE-2007-2447
 git clone https://github.com/m3m0o/chamilo-lms-unauthenticated-big-upload-rce-poc.git ./exploits/CVE-2023-4220
 git clone https://github.com/kimusan/pkwner.git ./exploits/CVE-2021-4034
+git clone https://github.com/nikn0laty/Exploit-for-Dolibarr-17.0.0-CVE-2023-30253.git ./exploits/CVE-2023-30253
+git clone https://github.com/MaherAzzouzi/CVE-2022-37706-LPE-exploit ./exploits/CVE-2022-37706

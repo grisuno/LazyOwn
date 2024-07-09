61519cbb6087a3aaa2304b24bb9f77cc80cc5b4c
gris <gris@localhost.localdomain>
2024-07-08 23:06:15 -0400

    little discover .sh

--- modules/internal_discover.sh
+++ modules/internal_discover.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 for i in $(seq 1 254); do
-    timeout 1 bash -c "ping -c 1 10.10.0.$i" &> /dev/null && echo "[+] host 10.10.0.$i is ACTIVE" &
-done; wait
+	timeout 1 bash -c "ping -c 1 10.10.0.$i" &>/dev/null && echo "[+] host 10.10.0.$i is ACTIVE" || echo "[-] host 10.10.0.$i in INACTIVE" &
+
+done
+wait

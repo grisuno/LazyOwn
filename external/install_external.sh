9d2651102ff0556ae161e4d540e9ca83cbc51c8f
gris <gris@localhost.localdomain>
2024-07-08 21:22:49 -0400

    1 new exploit

--- external/install_external.sh
+++ external/install_external.sh
@@ -4,3 +4,4 @@ git clone https://github.com/m3m0o/chamilo-lms-unauthenticated-big-upload-rce-po
 git clone https://github.com/kimusan/pkwner.git ./exploits/CVE-2021-4034
 git clone https://github.com/nikn0laty/Exploit-for-Dolibarr-17.0.0-CVE-2023-30253.git ./exploits/CVE-2023-30253
 git clone https://github.com/MaherAzzouzi/CVE-2022-37706-LPE-exploit ./exploits/CVE-2022-37706
+git clone https://github.com/Arinerron/CVE-2022-0847-DirtyPipe-Exploit.git ./exploits/CVE-2022-0847

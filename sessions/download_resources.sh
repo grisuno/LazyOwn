4267e663c2716b6ebc24f97c161afa1195ebb08e
gris <gris@localhost.localdomain>
2024-07-25 06:52:31 -0400

    HackTheBox https://app.hackthebox.com/teams/overview/6429 new revshell.c and machine powned xD axlle hard

--- sessions/download_resources.sh
+++ sessions/download_resources.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 wget https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Rubeus.exe
+wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1

1d8dd79b0bb7f1f1968385d8e0b11c7aaf06e7bc
gris <gris@localhost.localdomain>
2024-07-29 02:06:56 -0400

    HackTheBox https://app.hackthebox.com/teams/overview/6429 fix arguments little bug

--- sessions/download_resources.sh
+++ sessions/download_resources.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Rubeus.exe
 wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1
+wget https://raw.githubusercontent.com/PowerShellEmpire/PowerTools/master/PowerUp/PowerUp.ps1

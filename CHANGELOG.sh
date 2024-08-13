5fc574e9d1b13e734d82d09e7489f5b23c4d5818
gris <gris@localhost.localdomain>
2024-08-13 01:35:29 -0400

    docs(docs): El cierre de una imagen en la documentación estaba mal :)\n\ntambien se modifico el CHANGELOG.sh que chulada de script\n\n👽 LazyOwn on HackTheBox: https://app.hackthebox.com/teams/overview/6429  👽  https://www.reddit.com/r/LazyOwn/   👽  https://grisuno.github.io/LazyOwn/

--- CHANGELOG.sh
+++ CHANGELOG.sh
@@ -6,8 +6,40 @@ pandoc README.md -f markdown -t html -s -o README.html
 # Nombre del archivo de changelog
 CHANGELOG_FILE="CHANGELOG.md"
 
+# Opciones de tipo de commit
+echo "Selecciona el tipo de commit:"
+echo "1) feat"
+echo "2) feature"
+echo "3) fix"
+echo "4) hotfix"
+echo "5) refactor"
+echo "6) docs"
+echo "7) test"
+read -p "Introduce el número del tipo de commit: " TYPE_OPTION
+
+# Mapeo de opciones a los tipos de commit
+case $TYPE_OPTION in
+  1) TYPE="feat" ;;
+  2) TYPE="feature" ;;
+  3) TYPE="fix" ;;
+  4) TYPE="hotfix" ;;
+  5) TYPE="refactor" ;;
+  6) TYPE="docs" ;;
+  7) TYPE="test" ;;
+  *) echo "Opción no válida"; exit 1 ;;
+esac
+
 # Solicitar el mensaje del commit al usuario
-read -p "Introduce el mensaje del commit: " COMMIT_MESSAGE
+read -p "Introduce el mensaje del commit (subject): " SUBJECT
+
+# Solicitar el cuerpo del commit
+read -p "Introduce el cuerpo del commit (body): " BODY
+
+# Definir el footer fijo
+FOOTER="👽 LazyOwn on HackTheBox: https://app.hackthebox.com/teams/overview/6429  👽  https://www.reddit.com/r/LazyOwn/   👽  https://grisuno.github.io/LazyOwn/"
+
+# Formatear el mensaje del commit
+COMMIT_MESSAGE="${TYPE}(${TYPE}): ${SUBJECT}\n\n${BODY}\n\n${FOOTER}"
 
 # Obtener el último tag y el commit actual
 START_COMMIT=$(git describe --tags --abbrev=0)
@@ -39,6 +71,7 @@ echo "Changelog actualizado en $CHANGELOG_FILE"
 # Añadir el archivo de changelog al commit
 git add $CHANGELOG_FILE
 
+# Convertir el changelog a HTML
 pandoc $CHANGELOG_FILE -f markdown -t html -s -o CHANGELOG.html
 mv CHANGELOG.html docs/CHANGELOG.html
 git add docs/CHANGELOG.html

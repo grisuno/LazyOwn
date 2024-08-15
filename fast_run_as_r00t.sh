7101be0df17d3aea7fceff87de2fbef9f0f43982
gris <grisuno@gmail.com>
2024-08-14 23:00:31 -0400

    feature(cambios en script fast_run_as_r00t.sh): se agrega la opción --vpn <NUM> para poder elegir parametricamente que vpn se elije para la ejecucion \n\n Version: release/0.1.0 \n\n se agregan metodos de control en el flujo de los comandos como la salida de SIGNAL CONTROL + D \n\n  LazyOwn on HackTheBox: https://app.hackthebox.com/teams/overview/6429 \n\n  LazyOwn/   https://grisuno.github.io/LazyOwn/ \n\n \n\n Fecha: Wed Aug 14 18:20:13 2024 -0400 \n\n Hora: 1723674013

--- fast_run_as_r00t.sh
+++ fast_run_as_r00t.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
-# Nombre de la nueva sesión de tmux
 SESSION="lazyown_sessions"
 COMMAND='./run --no-banner'
-# Función para verificar y relanzar con sudo si es necesario
+VPN=1  
 check_sudo() {
     if [ "$EUID" -ne 0 ]; then
         echo "[S] Este script necesita permisos de superusuario. Relanzando con sudo..."
-        sudo bash "$0" "$@"
+        sudo "$0" --vpn "$VPN" "${@/#--vpn*/}"
         exit
     fi
 }
-
-check_sudo
-
-# Crear una nueva sesión de tmux
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --vpn)
+            if [[ "$2" =~ ^[0-9]+$ ]]; then
+                VPN=$2
+                shift
+            else
+                echo "Error: El valor para --vpn debe ser un número."
+                exit 1
+            fi
+            ;;
+        *)
+            echo "Error: Opción no reconocida $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+echo $VPN
+check_sudo "$@"
+echo $VPN
 tmux new-session -d -s $SESSION
-
-# Crear los primeros dos paneles (izquierda arriba e izquierda abajo)
 tmux send-keys -t $SESSION "sleep 5 && bash -c './run -c nmap'" C-m
 tmux split-window -v
 tmux send-keys -t $SESSION "sleep 5 && bash -c '$COMMAND'" C-m
 tmux split-window -v
 tmux send-keys -t $SESSION "sleep 60 && bash -c './run -c pyautomate'" C-m
-# Dividir el panel izquierdo (izquierda arriba) para crear los paneles derecho arriba y derecho abajo
 tmux select-pane -t 0
 tmux split-window -h
-
-# Agregar un delay para que la VPN tenga tiempo de conectarse
-
-tmux send-keys -t $SESSION "bash -c './run -c \"vpn 2\"'" C-m
+tmux send-keys -t $SESSION "bash -c './run -c \"vpn $VPN\"'" C-m
 tmux split-window -v
-
 tmux send-keys -t $SESSION "bash -c './run -c www'" C-m
-# Seleccionar el primer panel
 tmux select-pane -t 3
-
-# Adjuntar a la sesión
 tmux attach -t $SESSION

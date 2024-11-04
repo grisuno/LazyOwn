5b5772b597bef12a4c42837e65fd160ccdedc705
grisun0 <grisun0@proton.me>
2024-11-03 22:18:48 -0300

    too many commands documented at COMMANDS.md

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install ltrace
+sudo apt install ltrace python3-xyzservices python3-venv
 python3 -m venv env
 source env/bin/activate
 # pip install -r requirements.txt
@@ -28,36 +28,30 @@ pip install certipy-ad
 pip install ast 
 pip install pykeepass
 pip install cmd2
+pip install Pillow
 
 git clone https://github.com/grisuno/LazyOwnInfiniteStorage.git ./modules_ext/lazyown_infinitestorage
 chmod +x /modules_ext/lazyown_infinitestorage/install.sh
 
-
-# URL del archivo a descargar
 URL="https://raw.githubusercontent.com/grisuno/LazyOwnEncoderDecoder/main/lazyencoder_decoder.py"
 
-# Nombre del archivo de destino
 DEST_FILE="modules/lazyencoder_decoder.py"
 if [[ -f "$DEST_FILE" ]]; then
 	echo "El archivo $DEST_FILE ya existe. borrando..."
 	rm -rf "$DEST_FILE"
 fi
 
-# Función para descargar con wget
 download_with_wget() {
 	wget -O "$DEST_FILE" "$URL"
 }
 
-# Función para descargar con curl
 download_with_curl() {
 	curl -o "$DEST_FILE" "$URL"
 }
 
-# Comprobar si wget está instalado
 if command -v wget &>/dev/null; then
 	echo "wget encontrado. Descargando con wget..."
 	download_with_wget
-# Si wget no está instalado, comprobar si curl está instalado
 elif command -v curl &>/dev/null; then
 	echo "wget no encontrado. Descargando con curl..."
 	download_with_curl
@@ -66,7 +60,6 @@ else
 	exit 1
 fi
 
-# Comprobar si la descarga fue exitosa
 if [[ -f "$DEST_FILE" ]]; then
 	echo "Archivo descargado exitosamente: $DEST_FILE"
 else
